814244494f57f2104661c9e0bc3cb169b155189a
Israel Garate <israel.garate@tsoftlatam.com>
2022-07-12 16:35:39 -0400

    Initial commit

---
+++ src/test/java/PaginaWeb.java
@@ -0,0 +1,40 @@
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+
+public class PaginaWeb {
+    public static void main(String[] args) {
+        //inicializamos webdrivermanager con setup base y creamos el webDriver que aceptara el ChromeDriver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        //funciones configuracion -> funciones internas
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //funciones de navegacion -> funciones Externas
+        driver.get("https://www.google.com");
+        String tituloPagina = driver.getTitle(); // "Google"
+        //escribir en el tag input con nombre q
+        WebElement barraTextoGoogle = driver.findElement(By.name("q"));
+        barraTextoGoogle.sendKeys("TSOFT");
+        barraTextoGoogle.submit();
+
+        WebElement linkSitioTsoft = driver.findElement(By.xpath("//h3[contains(text(),'HOME')]"));
+        linkSitioTsoft.click();
+        String tituloPaginaTsoft = driver.getTitle();
+        System.out.println(tituloPaginaTsoft);
+        driver.close();
+    }
+}
